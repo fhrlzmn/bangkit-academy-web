7cd3e2fc51a5ba685a4cc9dabb1158de5adc703f
fhrlzmn <fhrlzmn26@gmail.com>
2023-02-23 21:42:17 +0700

    add toggle dark

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+const toggleDarkMode = document.querySelector('ul.nav-list input');
 const toggleHamburgerMenu = document.querySelector('.hamburger-menu input');
+
+const bangkitLogoElements = document.querySelectorAll('img.bangkit-logo');
+const htmlTagElement = document.querySelector('html');
 const headerElement = document.querySelector('header');
 const ulNavListElement = document.querySelector('ul.nav-list');
 const hamburgerBoxElement = document.querySelector('.hamburger-box');
 
+const moonIconElement = document.querySelector('.moon-icon');
+const sunIconElement = document.querySelector('.sun-icon');
+
 toggleHamburgerMenu.addEventListener('change', function (e) {
-  console.log(e.target.checked);
   if (e.target.checked) {
     headerElement.classList.add('active');
     ulNavListElement.classList.add('active');
@@ -15,3 +21,29 @@ toggleHamburgerMenu.addEventListener('change', function (e) {
     hamburgerBoxElement.classList.remove('active');
   }
 });
+
+toggleDarkMode.addEventListener('change', function (e) {
+  if (e.target.checked) {
+    htmlTagElement.classList.add('dark');
+    moonIconElement.classList.add('hidden');
+    sunIconElement.classList.remove('hidden');
+    changeLogoColor();
+  } else {
+    htmlTagElement.classList.remove('dark');
+    moonIconElement.classList.remove('hidden');
+    sunIconElement.classList.add('hidden');
+    changeLogoColor();
+  }
+});
+
+function changeLogoColor() {
+  if (htmlTagElement.classList.contains('dark')) {
+    for (const element of bangkitLogoElements) {
+      element.src = './assets/bangkit-logo-white.webp';
+    }
+  } else {
+    for (const element of bangkitLogoElements) {
+      element.src = './assets/bangkit-logo-black.webp';
+    }
+  }
+}
