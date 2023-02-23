f7313c435a7b47bffb249d55d2d572cd765af65d
fhrlzmn <fhrlzmn26@gmail.com>
2023-02-23 19:39:09 +0700

    responsive & hamburger-menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('it works!');
+const toggleHamburgerMenu = document.querySelector('.hamburger-menu input');
+const headerElement = document.querySelector('header');
+const ulNavListElement = document.querySelector('ul.nav-list');
+const hamburgerBoxElement = document.querySelector('.hamburger-box');
+
+toggleHamburgerMenu.addEventListener('change', function (e) {
+  console.log(e.target.checked);
+  if (e.target.checked) {
+    headerElement.classList.add('active');
+    ulNavListElement.classList.add('active');
+    hamburgerBoxElement.classList.add('active');
+  } else {
+    headerElement.classList.remove('active');
+    ulNavListElement.classList.remove('active');
+    hamburgerBoxElement.classList.remove('active');
+  }
+});
